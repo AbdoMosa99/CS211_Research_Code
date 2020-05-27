17eb3402c463710fe259a80717a781fcbdaa7a14
abdomosa1999 <37039899+abdomosa1999@users.noreply.github.com>
2020-05-27 23:29:55 +0200

    Update SNode.h

--- SinglyNode/SNode.h
+++ SinglyNode/SNode.h
@@ -3,12 +3,16 @@
 
 template <class T> class SinglyLinkedList;
 template <class T> class CircularLinkedList;
+template <class T> class LinkedStack;
+template <class T> class LinkedQueue;
 
 template <class T>
 class SingleNode
 {
 	friend SinglyLinkedList<T>;
 	friend CircularLinkedList<T>;
+	friend LinkedStack<T>;
+	friend LinkedQueue<T>;
 
 	T data;
 	SingleNode<T> *next;

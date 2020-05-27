e2e36312bf81a3db6c57b9156a0a920e5244a7fe
abdomosa1999 <37039899+abdomosa1999@users.noreply.github.com>
2020-05-27 23:30:24 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,15 @@
 void singleTest();
 void doubleTest();
 void circularTest();
+void stackTest();
+void queueTest();
+void binaryTreeTest();
 
 int main() {
 	singleTest();
 	doubleTest();
 	circularTest();
+	stackTest();
+	queueTest();
+	binaryTreeTest();
 }
-
